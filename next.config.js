6ad8976c0cfcb44420acb737ddbef8ab02d1ea38
Vishal <vishaljangid2@outlook.com>
2023-11-26 23:05:16 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
   distDir: "out",
+  assetPrefix : "/imageswift-docs/",
+  basePath: "/imageswift-docs"
 })
 
 module.exports = {
